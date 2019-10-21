f3fc6196a002628129f606b55a00aa6301bb8f4b
Aaron Campbell <aaron@keydesign.com>
2019-10-21 08:56:45 -0700

    hivolts

--- projects/hivolts/GameFrame.java
+++ projects/hivolts/GameFrame.java
@@ -12,29 +12,27 @@ public class GameFrame extends JComponent{
     public static int[][] objectLoc = new int[12][12]; // "playable" grid
     public static Npc[] npcLoc = new Npc[mhoCount]; // *magic*
     public int score; // for message
-    public int roundNum ; // for message
+    public int roundNum; // for message
     public String message = "Round: " + roundNum + " Score: " + score; // for win / lose message
-    public int deadMho = 0; // counts the number of dead mhos
     public boolean alive = true; // keeps track of whether or not the player is alive
     public boolean reset = false; // whether or not to reset game
-
+    private int deadMho;
     public void init(){ // called when game starts
         reset = false;
         for (int i = 0; i < 10; i++){
-          for (int j = 0; j < 10; j++){
-            objectLoc[i][j] = 0; // resets whole grid
-          }
+            for (int j = 0; j < 10; j++){
+                objectLoc[i][j] = 0; // resets whole grid
+            }
         }
-        if (alive = true){
-          roundNum++;
-        } else {
-          roundNum = 0;
-          score = 0;
+        if (alive == true){ // when round completes and player is still alive
+            roundNum++; // increase the round number by 1
+        } else { // when round completes and player is dead
+            roundNum = 1; // reset round counter
+            score = 0; // reset score
         }
         message = "Round: " + roundNum + " Score: " + score; // for win / lose message
         x = 1; // reset player x pos
         y = 1; // reset player y pos
-        deadMho = 0; // reset dead mho cound
         alive = true; // reset alive value
         objectLoc[1][1] = -1; // prevents fence / mho from spawn on player
         spawnFence();
@@ -53,12 +51,12 @@ public class GameFrame extends JComponent{
                 }
             }
         }
-      // boundries
-      for (int i=0; i<12; i++) { // sets borders to fences
-            objectLoc[0][i] = 1;
-            objectLoc[11][i] = 1;
-            objectLoc[i][0] = 1;
-            objectLoc[i][11] = 1;
+        // boundries
+        for (int i=0; i<12; i++) { // sets borders to fences
+            objectLoc[0][i] = 1; // left border
+            objectLoc[11][i] = 1; // right border
+            objectLoc[i][0] = 1; // top border
+            objectLoc[i][11] = 1; // bottom border
         }
     }
 
@@ -67,7 +65,7 @@ public class GameFrame extends JComponent{
             while(true){
                 int gridPosx = (int) (10 * Math.random()); // rand x
                 int gridPosy = (int) (10 * Math.random()); // rand y
-                if(objectLoc[gridPosx+1][gridPosy+1] == 0){
+                if(objectLoc[gridPosx+1][gridPosy+1] == 0){ // if (x,y) is open square
                     Npc name = new Npc(gridPosx+1, gridPosy +1); // new npc instance
                     npcLoc[i] = name; // assign location values
                     break;
@@ -76,7 +74,7 @@ public class GameFrame extends JComponent{
         }
     }
 
-    public void updateFrameSize() { // resize
+    public void updateFrameSize() { // resize (similar to flag)
         if(getWidth()>getHeight()){
             scale = getHeight()/14;
         } else {
@@ -90,10 +88,12 @@ public class GameFrame extends JComponent{
         //Paints npcs
         g.setColor(Color.RED);
         for(int i = 0; i < npcLoc.length; i++){
-            g.fillOval((int) (scale*(npcLoc[i].xpos+1.05)), (int) (scale*(npcLoc[i].ypos+1.05)), (int) (0.9*scale), (int) (0.9*scale));
+            if(npcLoc[i].alive){
+                g.fillOval((int) (scale*(npcLoc[i].xpos+1.05)), (int) (scale*(npcLoc[i].ypos+1.05)), (int) (0.9*scale), (int) (0.9*scale));
+            }
         }
 
-        // walls
+        // walls and borders
         for(int i=0; i<12; i++){
             for(int n=0; n<12; n++){
                 if(objectLoc[i][n] == 1){
@@ -162,53 +162,67 @@ public class GameFrame extends JComponent{
     public void jump(){ // moves player randomly but never on to fence
         x = (int) (10 * Math.random());
         y = (int) (10 * Math.random());
-        while (objectLoc[x][y] == 1) { // this is the never on fence part
-          x = (int) (10 * Math.random());
-          y = (int) (10 * Math.random());
+        while (objectLoc[x][y] == 1) { // this is the never on a fence part
+            x = (int) (10 * Math.random());
+            y = (int) (10 * Math.random());
         }
         checkWin();
     }
 
-    public void findNpcs() { // npc moves and check how many npc die
-        int oldDeadMho = deadMho;
+    public void findNpcs() { // npc moves and check how many npcs died
+         int oldDeadMho;
+        if(deadMho != npcLoc.length){ //Still playing same round
+            oldDeadMho = deadMho;
+        } else { // New round started
+            oldDeadMho = 0;
+        }
         deadMho = 0;
         for(int i = 0; i < npcLoc.length; i++) {
-            if (objectLoc[npcLoc[i].xpos][npcLoc[i].ypos] != 1){
-              npcLoc[i].npcMove(); // moves the npcs in order
+            if (npcLoc[i].alive){
+                npcLoc[i].npcMove(); // moves the npcs in order
             }
-            if (objectLoc[npcLoc[i].xpos][npcLoc[i].ypos] == 1){ // if mho is on fence
-              deadMho++; // adds to dead mho cound
+            if (!npcLoc[i].alive){
+                deadMho++; // adds to dead mho round
             }
         }
-        if (deadMho > oldDeadMho){
-          score = score + roundNum*(deadMho - oldDeadMho);
-        }
+        score += roundNum * (deadMho - oldDeadMho); // calculates score based of how many mhos died and on what round
         checkWin();
     }
 
     public void checkWin(){ // check if player is on mho or fence or if all mhos are dead
-      for(int i = 0; i < npcLoc.length; i++) {
-        if ((npcLoc[i].xpos == x && npcLoc[i].ypos == y) || objectLoc[x][y] == 1){
-          alive = false;
+        for(int i = 0; i < npcLoc.length; i++) {
+            if ((npcLoc[i].xpos == x && npcLoc[i].ypos == y) || objectLoc[x][y] == 1){
+                alive = false; // kills player
+                break;
+            }
         }
-      }
-      // messages
-      if (deadMho == mhoCount){ // if all mhos are dead
-        String s = "";
-        if (roundNum > 1){
-          s = "s";
+        // messages
+        if (mhosDead()){ // if all mhos are dead
+            String s = "";
+            if (roundNum > 1){
+                s = "s"; // for adding "s" to "round" if roundNum > 1
+            }
+            message = "Congrats! You have passed " + roundNum + " round" + s + " and have " + score + " points! Press any button to keep going!";
+            reset = true;
+        } else if (alive == false){ // if you are dead
+            message = "You Lost :( You died on round number " + roundNum + " with " + score + " points. Press any button to restart!";
+            reset = true;
+        } else { // if still alive and still mhos
+            message = "Round: " + roundNum + " Score: " + score;
+            alive = true;
+            reset = false;
         }
-        message = "You Won! You have passed " + roundNum + " round" + s + " and have " + score + " points! Press any button to keep going!";
-        reset = true;
-      } else if (alive == false){ // if you are dead
-        message = "You Lost :( You died on round number " + roundNum + " with " + score + " points. Press any button to restart!";
-        reset = true;
-      } else { // if still alive and still mhos
-        message = "Round: " + roundNum + " Score: " + score;
-        deadMho = 0;
-        alive = true;
-        reset = false;
-      }
-      repaint();
+        repaint();
+    }
+
+    public boolean mhosDead(){ // if all mhos are dead, go to next round
+        boolean retval = true;
+        for(int i = 0; i < npcLoc.length; i++){
+            if(npcLoc[i].alive){
+                retval = false;
+                break;
+            }
+        }
+        return retval;
     }
 }
--- projects/hivolts/Main.java
+++ projects/hivolts/Main.java
@@ -7,42 +7,38 @@ import java.awt.event.ComponentEvent;
 
 public class Main extends JFrame implements KeyListener {
     private GameFrame draw;
-    private String[] thing = {"Hey there, how's life. This value is never used so I thought I'd just say hi! :)"};
+    private String[] thing = {"Hey there, how's life. This value is used but not actually useful so I thought I'd just say hi! :)"};
 
     public void keyPressed(KeyEvent e) {
     }
 
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased(KeyEvent e) { // records keystrokes
         if (draw.reset == false){
-          if (e.getKeyCode() == KeyEvent.VK_D) {
-              draw.moveRight();
-          } else if (e.getKeyCode() == KeyEvent.VK_A) {
-              draw.moveLeft();
-          } else if (e.getKeyCode() == KeyEvent.VK_X) {
-              draw.moveDown();
-          } else if (e.getKeyCode() == KeyEvent.VK_W) {
-              draw.moveUp();
-          }
-          if (e.getKeyCode() == KeyEvent.VK_Q) {
-              draw.moveUpLeft();
-          } else if (e.getKeyCode() == KeyEvent.VK_E) {
-              draw.moveUpRight();
-          } else if (e.getKeyCode() == KeyEvent.VK_Z) {
-              draw.moveDownLeft();
-          } else if (e.getKeyCode() == KeyEvent.VK_C) {
-              draw.moveDownRight();
-          } else if (e.getKeyCode() == KeyEvent.VK_S) {
-              draw.stay();
-          } else if (e.getKeyCode() == KeyEvent.VK_J) {
-              draw.jump();
-          }
+            if (e.getKeyCode() == KeyEvent.VK_D) {
+                draw.moveRight();
+            } else if (e.getKeyCode() == KeyEvent.VK_A) {
+                draw.moveLeft();
+            } else if (e.getKeyCode() == KeyEvent.VK_X) {
+                draw.moveDown();
+            } else if (e.getKeyCode() == KeyEvent.VK_W) {
+                draw.moveUp();
+            }
+            if (e.getKeyCode() == KeyEvent.VK_Q) {
+                draw.moveUpLeft();
+            } else if (e.getKeyCode() == KeyEvent.VK_E) {
+                draw.moveUpRight();
+            } else if (e.getKeyCode() == KeyEvent.VK_Z) {
+                draw.moveDownLeft();
+            } else if (e.getKeyCode() == KeyEvent.VK_C) {
+                draw.moveDownRight();
+            } else if (e.getKeyCode() == KeyEvent.VK_S) {
+                draw.stay();
+            } else if (e.getKeyCode() == KeyEvent.VK_J) {
+                draw.jump();
+            }
         } else {
-          if(draw.alive == true){
-            draw.init();
-            draw.repaint();
-          } else {
-            main(thing);
-          }
+            draw.init(); // run init again
+            draw.repaint(); // refresh the screen
         }
     }
 
@@ -64,7 +60,8 @@ public class Main extends JFrame implements KeyListener {
     public static void main(String[] args) {
         Main frame = new Main();
         frame.setTitle("HiVolts");
-        frame.setSize(1000, 1000);
+        frame.pack();
+        frame.setSize(500, 500);
         frame.setMinimumSize(new Dimension(400, 400));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.draw.init();

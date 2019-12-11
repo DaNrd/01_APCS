ae8fdb59018bbd7f847c3aefcc58db80823fa188
Aaron Campbell <aaron@keydesign.com>
2019-12-11 13:35:05 -0800

    PCOP efficiency updates

--- projects/class/programmingContestOrientedProblem/src/Reader.java
+++ projects/class/programmingContestOrientedProblem/src/Reader.java
@@ -5,8 +5,8 @@ import java.text.DecimalFormat;
 
 public class Reader {
     public String fileLoc; // path of file
-    public int lineNum; // number of lines (starts at -1 for reasons)
-    public String[] input;
+    public int[] integers;
+    public String[] queries;
     public DecimalFormat df = new DecimalFormat("0.000");
 
     public void processInput() {
@@ -25,40 +25,29 @@ public class Reader {
 
     public void readFile() { // records the file in an ArrayList called input
         countLines();
-        setInput();
+//        setInput();
     }
 
     public void countLines() {
         try {
             Scanner scanner = new Scanner(new File(fileLoc));
-            while (scanner.hasNextLine()) {
-                lineNum++;
-                scanner.nextLine();
+            integers = new int[Integer.parseInt(scanner.nextLine())];
+            for(int i = 0; i<integers.length; i++) {
+                integers[i] = Integer.parseInt(scanner.nextLine());
             }
-            input = new String[lineNum];
+            queries = new String[Integer.parseInt(scanner.nextLine())];
+            for(int i = 0; i<queries.length; i++) {
+                queries[i] = scanner.nextLine();
+            }
             scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public void setInput() {
-        try {
-            Scanner scanner = new Scanner(new File(fileLoc));
-            lineNum = 0;
-            while (scanner.hasNextLine()) {
-                input[lineNum] = scanner.nextLine();
-                lineNum++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public void computeQueries() {
-        int qStart = Integer.parseInt(input[0])+1; // to skip first n+1 ints
-        for(int i = 0; i < Integer.parseInt(input[qStart]); i++){ // to process all queries
-            String[] currentQuerry = input[qStart + i + 1].split(" "); // splits querry into 2 strings (split at space)
+        for(int i = 0; i < queries.length; i++){ // to process all queries
+            String[] currentQuerry = queries[i].split(" "); // splits querry into 2 strings (split at space)
             int a = Integer.parseInt(currentQuerry[0]); // first number of query
             int b = Integer.parseInt(currentQuerry[1]); // second number of querry
             average(i, a, b); // calculates the average between the two terms
@@ -68,7 +57,7 @@ public class Reader {
     public void average(int i, int a, int b) {
         double sol = 0; // starts solution value at 0
         for(int j = 0; j < (b - a + 1); j++) {
-            sol += Integer.parseInt(input[a + j]); // adds the numbers between query values
+            sol += integers[a + j - 1];
         }
         System.out.println(df.format(sol/(b - a + 1))); // prints solution
     }

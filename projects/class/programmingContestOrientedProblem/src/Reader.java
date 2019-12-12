75b2ee06a4dc61000feda0d592574c1c842e6121
Aaron Campbell <aaron@keydesign.com>
2019-12-11 16:59:20 -0800

    PCOP efficiency update

--- projects/class/programmingContestOrientedProblem/src/Reader.java
+++ projects/class/programmingContestOrientedProblem/src/Reader.java
@@ -6,14 +6,12 @@ import java.text.DecimalFormat;
 public class Reader {
     public String fileLoc; // path of file
     public int[] integers;
-    public String[] queries;
     public DecimalFormat df = new DecimalFormat("0.000");
 
     public void processInput() {
         fileLoc = locateFile();
-        readFile();
         System.out.println("Solutions:");
-        computeQueries();
+        readFile();
     }
 
     public String locateFile() {
@@ -25,19 +23,19 @@ public class Reader {
 
     public void readFile() { // records the file in an ArrayList called input
         countLines();
-//        setInput();
     }
 
     public void countLines() {
         try {
             Scanner scanner = new Scanner(new File(fileLoc));
-            integers = new int[Integer.parseInt(scanner.nextLine())];
-            for(int i = 0; i<integers.length; i++) {
-                integers[i] = Integer.parseInt(scanner.nextLine());
+            integers = new int[Integer.parseInt(scanner.nextLine()) + 1];
+            integers[0] = 0;
+            for(int i = 1; i<integers.length; i++) {
+                integers[i] = integers[i-1] + Integer.parseInt(scanner.nextLine());
             }
-            queries = new String[Integer.parseInt(scanner.nextLine())];
-            for(int i = 0; i<queries.length; i++) {
-                queries[i] = scanner.nextLine();
+            scanner.nextLine();
+            while(scanner.hasNextLine()) {
+                computeQuery(scanner.nextLine());
             }
             scanner.close();
         } catch (FileNotFoundException e) {
@@ -45,20 +43,16 @@ public class Reader {
         }
     }
 
-    public void computeQueries() {
-        for(int i = 0; i < queries.length; i++){ // to process all queries
-            String[] currentQuerry = queries[i].split(" "); // splits querry into 2 strings (split at space)
-            int a = Integer.parseInt(currentQuerry[0]); // first number of query
-            int b = Integer.parseInt(currentQuerry[1]); // second number of querry
-            average(i, a, b); // calculates the average between the two terms
-        }
+    public void computeQuery(String query) {
+        String[] currentQuerry = query.split(" "); // splits querry into 2 strings (split at space)
+        int a = Integer.parseInt(currentQuerry[0]); // first number of query
+        int b = Integer.parseInt(currentQuerry[1]); // second number of querry
+        average(a, b); // calculates the average between the two terms
     }
 
-    public void average(int i, int a, int b) {
+    public void average(int a, int b) {
         double sol = 0; // starts solution value at 0
-        for(int j = 0; j < (b - a + 1); j++) {
-            sol += integers[a + j - 1];
-        }
-        System.out.println(df.format(sol/(b - a + 1))); // prints solution
+        sol = (double) (integers[b] - integers[a - 1])/(b - a + 1);
+        System.out.println(df.format(sol)); // prints solution
     }
 }

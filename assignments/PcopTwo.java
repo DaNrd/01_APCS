d61d981e658d402e57b080652f514864766baded
Aaron Campbell <aaron@keydesign.com>
2020-02-07 11:11:14 -0800

    first commit

--- assignments/PcopTwo.java
+++ assignments/PcopTwo.java
@@ -1,16 +1,18 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 public class PcopTwo {
 
-    public String fileLoc; // path of file
-    public float startTime; // time at start of execution
-    public float endTime; // time at end of execution
+    public static String fileLoc; // path of file
+    public static float startTime; // time at start of execution
+    public static float endTime; // time at end of execution
 
-    public ArrayList<ArrayList<Integer>> input = new ArrayList<>();
-    public ArrayList<Integer> output = new ArrayList<>();
+    public static ArrayList<ArrayList<Integer>> input = new ArrayList<>();
+    public static ArrayList<Integer> output = new ArrayList<>();
 
     public static void main(String[] args) {
         fileLoc = locateFile();
+        startTime = System.nanoTime();
         collectArrays();
         sortArrays();
         returnOutput();
@@ -18,14 +20,17 @@ public class PcopTwo {
     }
 
     public static void displayTimeTaken() {
-        System.out.println("Time taken = " + startTime - endTime);
+        endTime = System.nanoTime();
+        System.out.println("Time taken = " + (endTime - startTime)*Math.pow(10,(-9)));
     }
 
     public static void returnOutput() {
         System.out.println("Output:");
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < output.size(); i++) {
             System.out.println(output.get(i));
         }
+        System.out.println("N: " + input.get(0).get(0));
+        System.out.println("K: " + input.get(0).get(1));
     }
 
     public static String locateFile() {
@@ -48,36 +53,51 @@ public class PcopTwo {
     }
 
     public static void sortArrays() {
-        for(int i = 1; i < (input.size() - 1); i++) {
+        for(int i = 1; i < input.size(); i++) {
             output = merge(input.get(i), output);
         }
     }
 
     public static ArrayList<Integer> merge(ArrayList<Integer> one, ArrayList<Integer> two) {
         ArrayList<Integer> retval = new ArrayList<>();
-        for(int i = 1; i < one.size(); i++) {
-            for(int j = 1; j < two.size(); j++) {
-                if(one.get(i) < two.get(j)) {
-                    retval.add(one.get(i));
-                } else {
-                    retval.add(two.get(j));
-                }
+
+        while(one.size() > 1) {
+            if(two.size() == 0 || one.get(1) < two.get(0)) {
+                retval.add(one.get(1));
+                one.remove(1);
+            } else {
+                retval.add(two.get(0));
+                two.remove(0);
             }
         }
+        while(two.size() > 0) {
+            retval.add(two.get(0));
+            two.remove(0);
+        }
+//        for(int i = 1; i < one.size(); i++) {
+//            for(int j = 1; j < two.size(); j++) {
+//                if(one.get(i) < two.get(j)) {
+//                    retval.add(one.get(i));
+//                } else {
+//                    retval.add(two.get(j));
+//                }
+//            }
+//        }
         return retval;
     }
 
     public static ArrayList<Integer> readLine(String line) {
         ArrayList<Integer> retval = new ArrayList<>();
-        String value;
-        for(int i = 0; i < firstLine.length; i++) {
-            if(line.charAt(i).equals(' ')) {
+        String value = "";
+        for(int i = 0; i < line.length(); i++) {
+            if(line.charAt(i) == ' ') {
                 retval.add(Integer.parseInt(value));
                 value = "";
             } else {
-                value += firstLine.charAt(i);
+                value += line.charAt(i);
             }
         }
+        retval.add(Integer.parseInt(value));
         return retval;
     }
 }
